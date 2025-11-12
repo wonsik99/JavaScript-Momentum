c4cebfa5c846334369377d0a317f4e76296d7599
wonsik99 <algrthm@umich.edu>
2025-11-12 17:20:41 -0500

    Clock Created

--- js/greetings.js
+++ js/greetings.js
@@ -8,15 +8,15 @@ const USERNAME_KEY = "username"
 function onLoginSubmit(event) {
   event.preventDefault();
   loginForm.classList.add(HIDDEN_CLASSNAME);
-  const username = loginInput.value;
-  localStorage.setItem(USERNAME_KEY, username)
-  // greeting.innerText = "Hello " + username;
-  paintGreetings(username)
+  localStorage.setItem(USERNAME_KEY, loginInput.value)
+  paintGreetings()
 
 } 
 
-function paintGreetings(username) {
+function paintGreetings() {
+  const username = localStorage.getItem(USERNAME_KEY);
   greeting.innerText = `Hello ${username}`;
+  // greeting.innerText = "Hello " + username;
   greeting.classList.remove(HIDDEN_CLASSNAME);
 }
 
@@ -28,5 +28,5 @@ if (savedUsername === null) {
   loginForm.addEventListener("submit", onLoginSubmit);
 } else {
   //show the greetings
-  paintGreetings(savedUsername)
+  paintGreetings()
 }
